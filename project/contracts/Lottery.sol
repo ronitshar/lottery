e64cd5f36bfbf296279e1a9407713e92ce3bb9c3
ShivaanshK <shivaanshkapoor02@gmail.com>
2018-12-03 17:15:30 -0800

    Added player specifics to lottery contract

--- project/contracts/Lottery.sol
+++ project/contracts/Lottery.sol
@@ -1,11 +1,30 @@
 pragma solidity ^0.4.24;
 
+ // To test the app in truffle console: Lottery.deployed().then(function(instance) {app = instance})
+
 contract Lottery{
-    
-    string public player;
+
+    //components of a player
+    struct Player{
+        uint id;
+        string name;
+        uint amount;
+    }
+
+    //store player's id with corresponding name and amount
+    mapping(uint => Player) public players;
+
+    //player count
+    uint public count;
 
     function Lottery() public{
       //Constructor  
-        player = "Shiv";
+    }
+
+    function addPlayer(string _name) private{
+        count++;
+        players[count] = Player(count, _name, 0);
     }
 }
+
+
